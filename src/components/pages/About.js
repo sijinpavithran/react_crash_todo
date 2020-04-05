31da3c5235e97f8450013e106e21fd0547739777
Sijin Pavithran <sijinpavithran168@gmail.com>
2020-04-05 14:35:38 +0100

    React crash course practice

---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function () {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is a TodoList app v1.0. Part of react crash course</p>
+        </React.Fragment>
+    )
+}
